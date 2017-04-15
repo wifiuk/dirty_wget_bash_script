c4078735e7a595518f4428620f18fd54019edd43
wifiuk <wifiuk@users.noreply.github.com>
2017-04-15 22:38:25 +0100

    Update wget-script.sh

--- wget-script.sh
+++ wget-script.sh
@@ -1,1 +1,17 @@
-xx
+#!/bin/bash
+#change the numbers below to your number range
+for i in {1..888}
+do
+#user agent string
+user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
+#user agent command call
+usercmd="--user-agent= $user_agent"
+#calls a file called cookies.txt that is in the same directory as script
+cookies="--load-cookies cookies.txt"
+#output location of where you want to store file
+output_location="-O /home/USERNAME/Downloads/"
+        wget "$usercmd" $cookies $output_location/$i.zip "https://URLGOESHERE/?download="$i; 
+done
+
+
+
