3359f408adec5cdb0f145b6e4d07c4e5f439de68
Wifiuk <wifiuk@github.com>
2017-04-15 22:54:02 +0100

    modified:   wget-script.sh

--- wget-script.sh
+++ wget-script.sh
@@ -8,7 +8,7 @@ user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Fire
 usercmd="--user-agent= $user_agent"
 #calls a file called cookies.txt that is in the same directory as script
 cookies="--load-cookies cookies.txt"
-#output location of where you want to store file
+#output location of where you want to store the file
 output_location="-O /home/USERNAME/Downloads/"
         wget "$usercmd" $cookies $output_location/$i.zip "https://URLGOESHERE/?download="$i; 
 done
